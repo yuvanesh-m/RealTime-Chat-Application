744ca26a992698a86d6e1289a6334468f5eb4d9b
Yuvanesh M <yuvaleon89@gmail.com>
2026-09-21 19:49:45 +0530

    Changed the origins to support deployment

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
             registry.addEndpoint("/chat")
-                    .setAllowedOrigins("http://localhost:8080")
+                    .setAllowedOrigins("*")
                     .withSockJS();
     }
 
